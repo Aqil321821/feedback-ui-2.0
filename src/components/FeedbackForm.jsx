1ef308ab3cca63531d73662fa09594ac359a8972
Aqil321821 <aqilrao321821@gmail.com>
2024-11-11 18:46:03 +0500

    form work done

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,26 +1,55 @@
 import { useState } from 'react';
 
-
 import Card from './shared/Card';
-import Button from './shared/Button'; 
+import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
-function FeedbackForm() {
-    const [text,setText]=useState('')
+function FeedbackForm({handleAdd}) {
+  const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [msg, setMsg] = useState('');
+  const [rating, setRating] = useState(10);
 
-const handleChange=(e)=>{
-    setText(e.target.value)
-}
+  const handleChange = (e) => {
+    const inputValue = e.target.value; // input value directly le rahe hain
+    setText(inputValue);
+
+    if (inputValue === '') {
+      setBtnDisabled(true);
+      setMsg(null);
+    } else if (inputValue !== '' && inputValue.trim().length < 10) {
+      setBtnDisabled(true);
+      setMsg('Review must be at least 10 characters');
+    } else {
+      setMsg('Thanks For Your review , hit send button');
+      setBtnDisabled(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      handleAdd(newFeedback);
+      setText('')
+    }
+  };
 
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How Would you rate our services?</h2>
         {/* compt rating select */}
+        <RatingSelect select={(rating) => setRating(rating)} />
 
         <div className='input-group'>
-            <input type="text" placeholder='write a review'  value={text} onChange={handleChange}/>
-            <Button>Send</Button>
+          <input type='text' placeholder='write a review' value={text} onChange={handleChange} />
+          <Button isDisabled={btnDisabled}>Send</Button>
         </div>
+        {msg && <div className='message'>{msg}</div>}
       </form>
     </Card>
   );
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,10 +1,9 @@
-
-function Button({children , version='primary' ,type="submit", isDisabled=false}) {
+function Button({ children, version = 'primary', type = 'submit', isDisabled = false }) {
   return (
-     <button type={type} isDisabled={isDisabled}  className={`btn btn-${version}` }>
-               {children}
-     </button>
-  )
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+      {children}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
